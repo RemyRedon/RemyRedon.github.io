91907ab14f24eea27e94a4cb0a3030394330a715
RemyRedon <remy.redon@hotmail.fr>
2025-04-02 18:04:42 +0200

    Update config vite

--- vite.config.ts
+++ vite.config.ts
@@ -13,5 +13,13 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]',
+      },
+    },
   },
 });
