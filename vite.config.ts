f304c53a7c05ad1a8c8174477f35c1750f65ce9e
RemyRedon <remy.redon@hotmail.fr>
2025-04-02 13:18:03 +0200

    some changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: 'RemyRedon.github.io',
   plugins: [react()],
   css: {
     preprocessorOptions: {
       scss: {},
     },
   },
+  base: '/RemyRedon.github.io/',
 });
